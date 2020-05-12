d0afad6ecf63b0d8986eb08c334296bad0171056
Yash <yashayash123@gmail.com>
2020-05-12 22:08:04 +0530

    Scope and References

--- Software Engineering Foundations/Advanced Ruby/advanced_methods_exercise/01_coprime.rb
+++ Software Engineering Foundations/Advanced Ruby/advanced_methods_exercise/01_coprime.rb
@@ -3,28 +3,35 @@
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
 
+# refactor
+
 def coprime?(num_1, num_2)
-
-  num_1_divisors = (1...num_1).select do |div|
-    div if num_1 % div == 0
-  end
-
-  num_2_divisors = (1...num_2).select do |div|
-    div if num_2 % div == 0
-  end
-
-  common = []
-  common = num_1_divisors.select do |num|
-    num if num_2_divisors.include? num
-  end
-
-  if common.length == 1 && common.first == 1
-    return true
-  else
-    return false
-  end
-
+  (2..num_1).none? { |div| num_1 % div == 0 && num_2 % div == 0}
 end
+
+
+# def coprime?(num_1, num_2)
+
+#   num_1_divisors = (1...num_1).select do |div|
+#     div if num_1 % div == 0
+#   end
+
+#   num_2_divisors = (1...num_2).select do |div|
+#     div if num_2 % div == 0
+#   end
+
+#   common = []
+#   common = num_1_divisors.select do |num|
+#     num if num_2_divisors.include? num
+#   end
+
+#   if common.length == 1 && common.first == 1
+#     return true
+#   else
+#     return false
+#   end
+
+# end
 
 
  p coprime?(25, 12)    # => true
