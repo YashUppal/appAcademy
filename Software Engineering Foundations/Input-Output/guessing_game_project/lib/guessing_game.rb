db262bbbacbc6e17671874d7719a41078ea9b8d3
Yash <yashayash123@gmail.com>
2020-05-19 23:47:43 +0530

    add guessing game

---
+++ Software Engineering Foundations/Input-Output/guessing_game_project/lib/guessing_game.rb
@@ -0,0 +1,36 @@
+class GuessingGame
+  def initialize(min, max)
+    @secret_num = rand(min..max)
+    @num_attempts = 0
+    @game_over = false
+  end
+
+  def num_attempts
+    @num_attempts
+  end
+
+  def game_over?
+    @game_over
+  end
+
+  def check_num(num_guessed)
+    @num_attempts += 1
+    if num_guessed == @secret_num
+      @game_over = true
+      puts "you win"
+    elsif num_guessed > @secret_num
+      puts 'too big'
+    else
+      puts 'too small'
+    end
+  end
+
+  def ask_user
+    print "enter a number: "
+    input_num = gets.chomp.to_i # remove \n from input, and convert to int
+    self.check_num(input_num)
+  end
+
+
+
+end
