4dacebee461a17695b4fbc2d1329a8b67a610212
Yash <yashayash123@gmail.com>
2020-05-24 00:04:25 +0530

    add mastermind part 3

--- Software Engineering Foundations/Object Oriented Programming/mastermind_project/lib/mastermind.rb
+++ Software Engineering Foundations/Object Oriented Programming/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,20 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+    @secret_code = Code.random(length)
+  end
 
+  def print_matches(code_instance)
+    puts @secret_code.num_exact_matches(code_instance)
+    puts @secret_code.num_near_matches(code_instance)
+  end
+
+  def ask_user_for_guess
+    puts "Enter a code"
+    input = gets.chomp
+    generated = Code.from_string(input)
+    print_matches(generated)
+    generated.pegs == @secret_code.pegs
+  end
 end
