629ebe83a5ce45756c0e99661703a1eaa7ddeb6f
Yash <yashayash123@gmail.com>
2020-06-15 10:02:44 +0530

    add maze_solver project v0.1

--- Ruby/Enumerables & Debugging/maze_solver_project/maze_solver.rb
+++ Ruby/Enumerables & Debugging/maze_solver_project/maze_solver.rb
@@ -1,7 +1,3 @@
-
-
-# maze = IO.readlines("maze1.txt",chomp: true)
-
 def translate_maze(maze)
   traversable_maze = Array.new
   maze.each do |line|
@@ -15,6 +11,10 @@ def print_maze(maze)
     print line.join("")
     puts
   end
+end
+
+def og_start(maze)
+  $og_start = find_start(maze)
 end
 
 def find_start(maze)
@@ -33,18 +33,6 @@ def find_end(maze)
   end_idx
 end
 
-# def delta(index,maze)
-#   start_index = find_start(maze)
-#   # return ((index.first - start_index.first) + (index.last - start_index.last)).abs
-#   return Math.sqrt(((index.first - start_index.first)**2 + (index.last - start_index.last)**2))
-# end
-
-# def delta_origin(index,maze)
-#   start_index = find_start(maze)
-#   # return ((index.first - start_index.first) + (index.last - start_index.last)).abs
-#   return Math.sqrt(((index.first - start_index.first)**2 + (index.last - start_index.last)**2))
-# end
-
 def delta_close(index,maze)
   end_index = find_end(maze)
 
@@ -57,11 +45,7 @@ def sense_up(start_index,maze)
   y_index = start_index.first
   x_index = start_index.last
   while true
-    break if (maze[y_index][x_index]) == "*"
-
-    if maze[y_index][x_index] == " "
-      # maze[y_index][x_index] = "."
-    end
+    break if (maze[y_index][x_index]) == "*" || (maze[y_index][x_index]) == "E"
     y_index -= 1
   end
   # Prev index is returned
@@ -73,15 +57,6 @@ def sense_right(start_index,maze)
   x_index = start_index.last
   while true
     break if (maze[y_index][x_index]) == "*" || (maze[y_index][x_index]) == "E"
-    # break if (maze[y_index][x_index]) == "*"
-    
-    # if (maze[y_index][x_index] == "E")
-    #   return [y_index,x_index-1]
-    # end
-
-    if maze[y_index][x_index] == " "
-      # maze[y_index][x_index] = "."
-    end
     x_index += 1
   end
   return [y_index,x_index-1]
@@ -91,11 +66,7 @@ def sense_down(start_index,maze)
   y_index = start_index.first
   x_index = start_index.last
   while true
-    break if (maze[y_index][x_index]) == "*"
-
-    if maze[y_index][x_index] == " "
-      # maze[y_index][x_index] = "."
-    end
+    break if (maze[y_index][x_index]) == "*" || (maze[y_index][x_index]) == "E"
     y_index += 1
   end
   return [y_index-1, x_index]
@@ -104,13 +75,8 @@ end
 def sense_left(start_index,maze)
   y_index = start_index.first
   x_index = start_index.last
-
   while true
-    break if maze[y_index][x_index] == "*"
-
-    if maze[y_index][x_index] == " "
-      # maze[y_index][x_index] = "."
-    end
+    break if maze[y_index][x_index] == "*" || (maze[y_index][x_index]) == "E"
     x_index -= 1
   end
   return [y_index,x_index+1]
@@ -129,9 +95,7 @@ def opposite(input)
   end
 end
 
-
 def move(old_index,move_index,maze)
-  # maze[old_index.first][old_index.last] = "."
   maze[old_index.first][old_index.last] = " "
   maze[move_index.first][move_index.last] = "S"
 end
@@ -140,91 +104,105 @@ end
 def travel(maze)
   prev_move = ""
   while true
-  puts
-  print_maze(maze)
-  puts
-  start = find_start(maze)
+    puts
+    print_maze(maze)
+    puts
+    start = find_start(maze)
 
-  sense_hash = {
-    "sense_up" => 0,
-    "sense_down" => 0,
-    "sense_right" => 0,
-    "sense_left" => 0
-  }
+    sense_hash = {
+      "sense_up" => 0,
+      "sense_down" => 0,
+      "sense_right" => 0,
+      "sense_left" => 0
+    }
 
-  sense_hash.each do |key,value|
-    sensed_index = eval key + "(start,maze)"
-    sense_hash[key] = delta_close(sensed_index,maze) if sensed_index != start
-  end
+    sense_hash.each do |key,value|
+      sensed_index = eval key + "(start,maze)"
+      sense_hash[key] = delta_close(sensed_index,maze) if sensed_index != start
+    end
 
-  # print sense_hash
+    possible_navs = []
 
-  possible_navs = []
-
-  # possible_navs << sense_hash.select { |key,value| key if (!value.zero?) && !(value == prev_move && value == opposite(prev_move)) }
-
-  
-  # puts prev_move
-  # puts opposite(prev_move)
-
-  possible_navs << sense_hash.select do |key,value|
-    if !value.zero?
-      if key != prev_move
-        if key != opposite(prev_move)
-          key
+    possible_navs << sense_hash.select do |key,value|
+      if !value.zero?
+        if key != prev_move
+          if key != opposite(prev_move)
+            key
+          end
         end
       end
     end
+
+    possible_navs = possible_navs.first.sort_by {|key,value| value }
+
+    if possible_navs == []
+      return maze
+      break
+    end
+
+
+    operation = possible_navs.first.first
+    move_index = eval operation + "(start,maze)"
+    prev_move = operation
+    move(start,move_index,maze)
+    sleep(0.7)
+
   end
-
-  
-
-
-
-  possible_navs = possible_navs.first.sort_by {|key,value| value }
-
-  if possible_navs == []
-    break
-  end
-
-  # print sense_hash
-  # print possible_navs
-
-
-
-  operation = possible_navs.first.first
-  move_index = eval operation + "(start,maze)"
-  # print move_index
-  puts
-  prev_move = operation
-  move(start,move_index,maze)
-  sleep(1)
 end
 
+def backtrace(maze)
+  puts "backtraced"
+  prev_move = ""
+  while true
+    puts
+    print_maze(maze)
+    puts
+    start = find_start(maze)
+
+    sense_hash = {
+      "sense_up" => 0,
+      "sense_down" => 0,
+      "sense_right" => 0,
+      "sense_left" => 0
+    }
+
+    sense_hash.each do |key,value|
+      sensed_index = eval key + "(start,maze)"
+      sense_hash[key] = delta_close(sensed_index,maze) if sensed_index != start
+    end
+
+    possible_navs = []
+
+    possible_navs << sense_hash.select do |key,value|
+      if !value.zero?
+        if key != prev_move
+          if key != opposite(prev_move)
+            key
+          end
+        end
+      end
+    end
+
+    possible_navs = possible_navs.first.sort_by {|key,value| value }
+
+    if possible_navs == []
+      break
+    end
+
+    operation = possible_navs.last.first
+    move_index = eval operation + "(start,maze)"
+    prev_move = operation
+    move(start,move_index,maze)
+
+    if move_index == $og_start
+      print_maze(maze)
+      sleep(0.7)
+      break
+    end
+
+    sleep(0.7)
+  end
 end
-
-# traversable = translate_maze(maze)
-# travel(traversable)
-# print_maze(traversable)
-# start = find_start(traversable)
-# p delta_close(sense_up(find_start(traversable),maze),maze)
-
-# delta_close(sense_right(start,traversable),traversable)
-# print_maze(traversable)
-
-# start = sense_up(start,traversable)
-# print_maze(traversable)
-
-# start = sense_right(start,traversable)
-# print_maze(traversable)
-
-# start = sense_down(start,traversable)
-# print_maze(traversable)
-
-# start = sense_right(start,traversable)
-# print_maze(traversable)
-
-# p delta_close([6,9],traversable) 
 
 if __FILE__ == $PROGRAM_NAME
   puts
@@ -232,7 +210,11 @@ if __FILE__ == $PROGRAM_NAME
   puts
   input = gets.chomp
 
-  # maze = IO.readlines("maze1.txt",chomp: true)
   maze = IO.readlines(input,chomp: true)
-  travel(translate_maze(maze))
+  og_start(maze)
+  maze = travel(translate_maze(maze))
+
+  print "Do you want to backtrace? (y/n) :"
+  back = gets.chomp
+  backtrace(maze) if back.downcase == "y" 
 end
