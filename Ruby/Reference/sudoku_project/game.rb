a5bf0f07a82111dfda8695b885d9fb3d860939bd
Yash <yashayash123@gmail.com>
2020-07-05 01:05:56 +0530

    add sudoku input validation

--- Ruby/Reference/sudoku_project/game.rb
+++ Ruby/Reference/sudoku_project/game.rb
@@ -10,19 +10,30 @@ class Game
   end
 
   def play
-  # main play loop
-  system("clear")
-  print ColorizedString["Welcome to Sudoku!\n\n"].colorize(:light_cyan)
-  sleep(3)
-  until board.solved?
+    # main play loop
     system("clear")
-    puts ColorizedString["Sudoku"].colorize(:light_cyan)
-    self.board.render
-    print "Enter the position you would like to change (e.g 1,2) : "
-    pos = gets.chomp.split(",").map(&:to_i)
-    print "\n\nEnter a value as well : "
-    val = gets.chomp.to_i
-    print "\nInvalid Position :(\n";sleep(1) if !self.board.update_at(val,pos)
+    print ColorizedString["Welcome to Sudoku!\n\n"].colorize(:light_cyan)
+    sleep(3)
+    until board.solved?
+      system("clear")
+      puts ColorizedString["Sudoku"].colorize(:light_cyan)
+      self.board.render
+      print "Enter the position you would like to change (e.g 1,2) : "
+      pos = gets.chomp.split(",").map(&:to_i)
+      print "\n\nEnter a value as well : "
+      val = gets.chomp.to_i
+      if self.valid_input?(val,pos)
+        print "\nInvalid Position :(\n";sleep(1) if !self.board.update_at(val,pos)
+      else
+        print "\nInvalid Input :(\n"
+        sleep(1)
+      end
     end
+  end
+
+  def valid_input?(val,pos)
+    return true if (pos.first > 0 && pos.first < 9) && (pos.last > 0 && pos.last < 9)
+    return true if val > 0 && val < 9
+    return false
   end
 end
