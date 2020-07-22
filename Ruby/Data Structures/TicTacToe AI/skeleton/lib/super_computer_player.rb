904ca93a1078a603c2367d5e72a1806da202b5da
Yash <yashayash123@gmail.com>
2020-07-22 15:41:33 +0530

    add TicTacToe AI

--- Ruby/Data Structures/TicTacToe AI/skeleton/lib/super_computer_player.rb
+++ Ruby/Data Structures/TicTacToe AI/skeleton/lib/super_computer_player.rb
@@ -2,6 +2,21 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board,mark)
+
+    node.children.each do |child_node|
+      if child_node.winning_node?(mark)
+        return child_node.prev_move_pos
+      end
+    end
+
+    node.children.each do |child_node|
+      if child_node.losing_node?(mark) == false
+        return child_node.prev_move_pos
+      end
+    end
+
+    raise "No winning or losing nodes"
   end
 end
 
--- Ruby/Data Structures/TicTacToe AI/skeleton/lib/tic_tac_toe_node.rb
+++ Ruby/Data Structures/TicTacToe AI/skeleton/lib/tic_tac_toe_node.rb
@@ -1,17 +1,91 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos                          
   end
 
   def losing_node?(evaluator)
+
+    if self.board.tied?
+      return false
+    end
+
+    if self.board.over?
+      if self.board.winner != evaluator 
+        return true
+      elsif (self.board.winner == evaluator) || (self.board.winner == nil)
+        return false
+      end
+    end
+
+    if self.alternate_mark == evaluator
+      # opponents
+      self.children.any? { |node| node.losing_node?(evaluator) }  
+    else
+      # players
+      self.children.all? { |node| node.losing_node?(evaluator) } 
+    end
+
   end
 
   def winning_node?(evaluator)
+    if self.board.over?
+      if self.board.winner == evaluator
+        return true
+      else
+        return false
+      end
+    end
+
+    if self.next_mover_mark == evaluator
+      self.children.any? { |node| node.winning_node?(evaluator) }
+    else
+      self.children.all? { |node| node.winning_node?(evaluator) }
+    end
+
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children_nodes = Array.new
+    i = 0
+    while i < self.board.rows.length
+      j = 0
+      while j < self.board.rows.length
+        if self.board.empty?([i,j])
+          duped_board = self.board.dup
+          duped_board.[]=([i,j],self.next_mover_mark) 
+          duped_node = self.class.new(duped_board,self.alternate_mark,[i,j])
+          children_nodes << duped_node
+        end
+        j += 1
+      end
+      i += 1
+    end
+    return children_nodes
   end
+
+  def alternate_mark
+    if self.next_mover_mark == :o
+      return :x
+    else
+      return :o
+    end
+  end
+
+  def alt_player_opp(evaluator)
+    if evaluator == :o
+      return :x
+    else
+      return :o
+    end
+  end
+
 end
