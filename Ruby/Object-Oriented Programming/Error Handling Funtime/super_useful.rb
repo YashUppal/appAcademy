8781f0323ece1e245849c258ff58af51e3cb258a
Yash <yashayash123@gmail.com>
2020-07-26 22:05:05 +0530

    add error handling funtime project

--- Ruby/Object-Oriented Programming/Error Handling Funtime/super_useful.rb
+++ Ruby/Object-Oriented Programming/Error Handling Funtime/super_useful.rb
@@ -1,16 +1,22 @@
 # PHASE 2
 def convert_to_int(str)
-  Integer(str)
+  begin
+    Integer(str)
+  rescue ArgumentError => e
+    nil
+  end
 end
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
 
 def reaction(maybe_fruit)
-  if FRUITS.include? maybe_fruit
+  if maybe_fruit == "coffee"
+    raise TypeError
+  elsif FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
   else 
-    raise StandardError 
+    raise ArgumentError 
   end 
 end
 
@@ -18,16 +24,44 @@ def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
   puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  begin
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue ArgumentError => e
+    puts "Not a fruit :("
+  rescue TypeError => e
+    puts "Yummmm..Coffee :D"
+    retry
+  end
 end  
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
-    @name = name
-    @yrs_known = yrs_known
-    @fav_pastime = fav_pastime
+
+    begin
+      raise ArgumentError if yrs_known < 5
+      @yrs_known = yrs_known
+    rescue
+      puts "Timeframe too short :("
+      exit
+    end
+
+    begin
+      raise ArgumentError if name.length <= 0
+      @name = name
+    rescue
+      puts "No Name Friend ? :("
+      exit
+    end
+
+    begin
+      raise ArgumentError if fav_pastime.length <= 0
+      @fav_pastime = fav_pastime
+    rescue
+      puts "No favorite pastime? :("
+      exit
+    end
   end
 
   def talk_about_friendship
