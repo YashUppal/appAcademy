dad96d5cfaa55a150d4a73a414e8142ff6991440
Yash <yashayash123@gmail.com>
2020-08-09 00:49:45 +0530

    add towers of hanoi specs and passing code

--- Ruby/RSpec and TDD/First TDD Projects/lib/first_tdd_projects.rb
+++ Ruby/RSpec and TDD/First TDD Projects/lib/first_tdd_projects.rb
@@ -23,5 +23,66 @@ class Array
     return pairs
   end
 
-  
+  def my_transpose
+    transposed = []
+    i = 0
+    while i < self.length
+      j = 0
+      row = []
+      while j < self.length
+        row << self[j][i]
+        j += 1
+      end
+      transposed << row
+      i += 1
+    end
+    return transposed
+  end
+end
+
+class TowerOfHanoi
+  attr_reader :pile_one,:pile_two,:pile_three
+
+  def initialize
+    @pile_one, @pile_two, @pile_three = Array.new([4,3,2,1]), Array.new, Array.new
+  end
+
+  def move(from,to)
+    from_disc = eval "self.#{from}.last"
+    to_disc = eval "self.#{to}.last"
+
+    if !to_disc.nil?
+      raise ArgumentError if to_disc < from_disc
+    end
+
+    eval "self.#{to}.push(self.#{from}.pop)"
+  end
+
+  def won?
+    self.pile_one == [4,3,2,1] || self.pile_two == [4,3,2,1] || self.pile_three == [4,3,2,1]
+  end
+
+  def render
+    print "#{self.pile_one}\n"
+    print "#{self.pile_two}\n"
+    print "#{self.pile_three}\n"
+  end
+
+  def play
+    system("clear")
+    puts "Welcome to Towers Of Hanoi!"
+    sleep(3)
+    while true
+      self.render
+      puts "Enter your 'from' and 'to' pile (e.g 'pile_one pile_two'): "
+      input = gets.chomp.split(" ")
+      self.move(input.first,input.last)
+      if self.won?
+        puts "You've won!!"
+        sleep(3)
+        break
+      end
+      system("clear")
+    end
+  end
 end
--- Ruby/RSpec and TDD/First TDD Projects/spec/first_tdd_projects_spec.rb
+++ Ruby/RSpec and TDD/First TDD Projects/spec/first_tdd_projects_spec.rb
@@ -23,4 +23,51 @@ describe Array do
       expect(arr.two_sum).to eq([[0,4],[2,3]])
     end
   end
+
+  describe "#my_transpose" do
+    subject(:arr) { Array.new([[0,1,2],[3,4,5],[6,7,8]]) }
+    it "is called on an array" do
+      expect {arr.my_transpose}.to_not raise_error
+    end
+
+    it "transpose the given matrix" do
+      expect(arr.my_transpose).to eq([[0,3,6],[1,4,7],[2,5,8]])
+    end
+  end
+end
+
+describe TowerOfHanoi do
+  describe "#initialize" do
+    subject(:tower) { TowerOfHanoi.new }
+    it "initializes three piles" do
+      expect(tower.pile_one).to eq([4,3,2,1])
+      expect(tower.pile_two).to be_empty
+      expect(tower.pile_three).to be_empty
+    end
+  end
+
+  describe "#move" do
+    subject(:tower) { TowerOfHanoi.new }
+    it "moves topmost disc from one pile to another" do
+      expect(tower.pile_one.last).to eq(1)
+      tower.move("pile_one","pile_two")
+      expect(tower.pile_one.last).to eq(2)
+      expect(tower.pile_two.last).to eq(1)
+    end
+    
+    it "raises error if trying to place bigger disc on smaller" do
+      tower.move("pile_one","pile_two")
+      expect{tower.move("pile_one","pile_two")}.to raise_error(ArgumentError)
+    end
+  end
+  
+  describe "#won?" do
+  subject(:tower) { TowerOfHanoi.new }
+  it "checks whether any pile is correctly arranged" do
+    tower.move("pile_one","pile_two")
+    expect(tower.won?).to be false
+    tower.move("pile_two","pile_one")
+    expect(tower.won?).to be true
+    end
+  end
 end
