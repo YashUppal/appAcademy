aeb98ca37dc3759e0446229715b758be4913d3df
Yash <yashayash123@gmail.com>
2020-08-17 12:48:30 +0530

    refactor deck code and spec

--- Ruby/RSpec and TDD/Poker/lib/deck.rb
+++ Ruby/RSpec and TDD/Poker/lib/deck.rb
@@ -1,3 +1,5 @@
+require_relative 'card.rb'
+
 class Deck
 
   attr_reader :deck
@@ -9,7 +11,49 @@ class Deck
   end
 
   def populate
-    @deck = ["🂡","🂢", "🂣", "🂤", "🂥", "🂦", "🂧", "🂨", "🂩", "🂪", "🂫", "🂭", "🂮","🂱", "🂲", "🂳", "🂴", "🂵", "🂶", "🂷", "🂸", "🂹", "🂺", "🂻", "🂽", "🂾","🃁", "🃂", "🃃", "🃄", "🃅", "🃆", "🃇", "🃈", "🃉", "🃊", "🃋", "🃍", "🃎","🃑", "🃒", "🃓", "🃔", "🃕", "🃖", "🃗", "🃘", "🃙", "🃚", "🃛", "🃝", "🃞"]
+    # spades
+    @deck << Card.new(:spade,:ace)
+    @deck << Card.new(:spade,:jack)
+    @deck << Card.new(:spade,:queen)
+    @deck << Card.new(:spade,:king)
+    i = 2
+    while i <= 10
+      @deck << Card.new(:spade, i)
+      i += 1
+    end
+
+    # diamonds
+    @deck << Card.new(:diamond,:ace)
+    @deck << Card.new(:diamond,:jack)
+    @deck << Card.new(:diamond,:queen)
+    @deck << Card.new(:diamond,:king)
+    i = 2
+    while i <= 10
+      @deck << Card.new(:diamond, i)
+      i += 1
+    end
+
+    # hearts
+    @deck << Card.new(:heart,:ace)
+    @deck << Card.new(:heart,:jack)
+    @deck << Card.new(:heart,:queen)
+    @deck << Card.new(:heart,:king)
+    i = 2
+    while i <= 10
+      @deck << Card.new(:heart, i)
+      i += 1
+    end
+    
+    # spades
+    @deck << Card.new(:spade,:ace)
+    @deck << Card.new(:spade,:jack)
+    @deck << Card.new(:spade,:queen)
+    @deck << Card.new(:spade,:king)
+    i = 2
+    while i <= 10
+      @deck << Card.new(:heart, i)
+      i += 1
+    end
   end
 
   def shuffle
