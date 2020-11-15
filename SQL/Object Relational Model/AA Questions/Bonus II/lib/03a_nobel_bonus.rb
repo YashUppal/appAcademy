56452a91b94fe98346b35248e527260965c46451
Yash <yashayash123@gmail.com>
2020-11-15 17:54:32 +0530

    add Bonus II (Entire SQLzoo rerun)

---
+++ SQL/Object Relational Model/AA Questions/Bonus II/lib/03a_nobel_bonus.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: nobels
+#
+#  yr          :integer
+#  subject     :string
+#  winner      :string
+
+require_relative './sqlzoo.rb'
+
+def physics_no_chemistry
+  # In which years was the Physics prize awarded, but no Chemistry prize?
+  execute(<<-SQL)
+  SELECT
+    DISTINCT(yr)
+  FROM
+    (
+    SELECT
+      yr
+    FROM
+      nobels
+    WHERE
+      subject in ('Physics')
+    ) AS foo
+  WHERE
+    yr NOT IN (
+      SELECT
+        yr
+      FROM
+        nobels
+      WHERE
+        subject = 'Chemistry'
+        )
+    
+  SQL
+end
