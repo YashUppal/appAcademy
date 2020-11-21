d07e2128bf22779d3d7e24e8c5a8b4885a54357b
Yash <yashayash123@gmail.com>
2020-11-22 00:59:35 +0530

    add Associations Exercise

--- SQL/Active Record/Associations Exercise/skeleton/app/models/course.rb
+++ SQL/Active Record/Associations Exercise/skeleton/app/models/course.rb
@@ -1,2 +1,33 @@
 class Course < ApplicationRecord
+  has_many(:enrollments,{
+    class_name: :Enrollment,
+    primary_key: :id,
+    foreign_key: :course_id
+  })  
+
+  has_many(:enrolled_students, {
+    through: :enrollments,
+    source: :user
+  }
+  )
+
+  has_one(:prerequisite,{
+    class_name: :Course,
+    primary_key: :prereq_id,
+    foreign_key: :id
+  }
+)
+
+  has_one(:instructor_id,{
+    class_name: :User,
+    primary_key: :instructor_id,
+    foreign_key: :id
+  }
+  )
+
+  has_one(:instructor,{
+    class_name: :User,
+    foreign_key: :id,
+    primary_key: :instructor_id
+  })
 end
--- SQL/Active Record/Associations Exercise/skeleton/app/models/enrollment.rb
+++ SQL/Active Record/Associations Exercise/skeleton/app/models/enrollment.rb
@@ -1,2 +1,15 @@
 class Enrollment < ApplicationRecord
+  belongs_to(:user,{
+    class_name: :User,
+    primary_key: :id,
+    foreign_key: :student_id
+  }
+  )
+
+  belongs_to(:course,{
+    class_name: :Course,
+    primary_key: :id,
+    foreign_key: :course_id
+  }
+  )
 end
