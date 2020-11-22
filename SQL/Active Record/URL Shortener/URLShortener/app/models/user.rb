fff1314b228567dad40391078197691362590ef2
Yash <yashayash123@gmail.com>
2020-11-22 18:31:21 +0530

    add url shortener phase 2

--- SQL/Active Record/URL Shortener/URLShortener/app/models/user.rb
+++ SQL/Active Record/URL Shortener/URLShortener/app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
+
+  has_many(:submitted_urls,{
+    class_name: :ShortenedUrl,
+    primary_key: :id,
+    foreign_key: :user_id
+  })
 end
--- SQL/Active Record/URL Shortener/URLShortener/db/schema.rb
+++ SQL/Active Record/URL Shortener/URLShortener/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_120303) do
+ActiveRecord::Schema.define(version: 2020_11_22_121509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shortened_urls", force: :cascade do |t|
+    t.string "long_url", null: false
+    t.string "short_url", null: false
+    t.integer "user_id", null: false
+    t.index ["short_url"], name: "index_shortened_urls_on_short_url", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
