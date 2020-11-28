e8289543ab0a297fc90609df39df55903cc5526d
Yash <yashayash123@gmail.com>
2020-11-28 22:44:03 +0530

    add movie buff project part 1

--- SQL/Active Record II/Movie Buff/movie_buff/01_queries.rb
+++ SQL/Active Record II/Movie Buff/movie_buff/01_queries.rb
@@ -7,6 +7,7 @@ def it_was_ok
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
 
+  Movie.select("id,title,score").where(score: (2..3))
 end
 
 def harrison_ford
@@ -20,6 +21,9 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
+
+  # Movie.joins(:castings).where("castings.actor_id = ? AND castings.ord != ?",(Actor.find_by(name: 'Harrison Ford').id),1).select("movies.id,movies.title")
+  Movie.joins(:actors).select("movies.id, movies.title").where("actors.name = 'Harrison Ford' AND castings.ord != ?",1)
 
 end
 
@@ -38,6 +42,7 @@ def biggest_cast
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
 
+  Movie.joins(:castings).select("movies.id,movies.title").group('movies.id').order("COUNT(movies.id) DESC").limit(3)
 end
 
 def directed_by_one_of(them)
@@ -53,6 +58,8 @@ def directed_by_one_of(them)
   #
   # Find the id and title of all the movies directed by one of 'them'.
 
+  Movie.joins(:director).select("movies.id,movies.title").where("actors.name IN (?)", them)
+
 end
 
 def movie_names_before_1940
@@ -67,4 +74,5 @@ def movie_names_before_1940
   #
   # Use pluck to find the title of all movies made before 1940.
 
+  Movie.where('yr < 1940').pluck(:title)
 end
