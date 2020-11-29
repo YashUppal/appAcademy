518ecc45f9bf0d1230de4020da913167a6d2231c
Yash <yashayash123@gmail.com>
2020-11-30 00:44:16 +0530

    change response associations from has_one to belongs_to

--- SQL/Active Record II/polls_app/app/models/response.rb
+++ SQL/Active Record II/polls_app/app/models/response.rb
@@ -1,5 +1,5 @@
 class Response < ApplicationRecord
 
-    has_one :respondent, class_name: :User, primary_key: :user_id, foreign_key: :id
-    has_one :answer_choice, class_name: :AnswerChoice, primary_key: :answer_choice_id, foreign_key: :id
+    belongs_to :respondent, class_name: :User, primary_key: :id, foreign_key: :user_id
+    belongs_to :answer_choice, class_name: :AnswerChoice, primary_key: :id, foreign_key: :answer_choice_id
 end
